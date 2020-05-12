aad14ea18b1b1870ba1509aa961970c85c6e9977
Ali Soomar <alisoomar@gmail.com>
2020-05-12 02:04:39 -0500

    Add Fire to tracked filed

---
+++ Fire.js
@@ -0,0 +1,39 @@
+import firebase from 'firebase';
+import * as Analytics from 'expo-firebase-analytics'; 
+import * as app from './app.json';
+
+class Fire {
+    constructor() {
+        firebase.initializeApp(app.expo.web.config.firebase);
+        this.observeAuth();
+    }
+
+    observeAuth = () => {
+        firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
+    }
+
+    onAuthStateChanged = user => {
+        if (!user) {
+            try {
+                firebase.auth().signInAnonymously();
+            } catch ({ message }) {
+                console.log(message);
+            }
+        }
+    }
+
+    getRef = (reference) => {
+        return firebase.database().ref(reference);
+    }
+
+    off() {
+        this.ref.off();
+    }
+
+    async logEvent(eventName, properties) {
+        await Analytics.logEvent(eventName, properties);
+    }
+}
+
+Fire.db = new Fire();
+export default Fire;
